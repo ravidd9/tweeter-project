84cbe37df479a9964b9ddb95d19bc3885e92db67
Ravid Cohen <ravidd9@gmail.com>
2019-03-20 13:11:38 +0200

    20.3 end day, twiter quest finished

--- main.js
+++ main.js
@@ -3,10 +3,30 @@ const renderer = Renderer()
 renderer.renderPosts(tweeter.getPosts()) 
 
 
-// $("i").on("click", function(){  //not working
-//     console.log($(this).attr("id"))
-//     let postID = 1 //find how to get it
-//     let commentID = $(this).attr("id").replace("i","c")
-//     tweeter.removeComment(postID,commentID)
-//     renderer.renderPosts(tweeter.getPosts())
-// })
+$("#container").on("click", "i", function(){ 
+    let postID = $(this).closest(".post").attr("id")
+    let commentID = $(this).closest(".comment").attr("id")
+    tweeter.removeComment(postID,commentID)
+    renderer.renderPosts(tweeter.getPosts())
+})
+$("#container").on("click", ".comButton", function(){
+    let text = $(this).closest(".post").find(".comInput").val()
+    if (text != ""){
+        tweeter.addComment(text, $(this).closest(".post").attr("id"))
+        renderer.renderPosts(tweeter.getPosts())
+        
+    }
+})
+$("#container").on("click", "#post", function(){
+    let text = $(this).siblings("#input").val()
+    console.log("asd")
+    if (text != ""){
+        tweeter.addPost(text)
+        renderer.renderPosts(tweeter.getPosts())
+    }
+})
+$("#container").on("click", ".postDel", function(){
+    let postID = $(this).closest(".post").attr("id")
+    tweeter.removePost(postID)
+    renderer.renderPosts(tweeter.getPosts())
+})
--- render.js
+++ render.js
@@ -2,12 +2,15 @@ const Renderer = function () {
     const renderPosts = function(posts){
         $("#posts").empty()
         for(let index in posts){
-            let newPost = $(`<div class='post' id=${posts[index].id}>${posts[index].text}</div>`)
-            $("#posts").append(newPost)
+            $("#posts").append(`<div class='post' id=${posts[index].id}>${posts[index].text}</div>`)
             for(let index2 in posts[index].comments){
                 let newComment = $(`<div class='comment' id=${posts[index].comments[index2].id}><i class="fas fa-minus-circle" id=i${posts[index].comments[index2].id.slice(1)}></i>${posts[index].comments[index2].text}</div>`)
                 $(`#${posts[index].id}`).append(newComment)
             }
+            $(`#${posts[index].id}`).append(`<input class="comInput" placeholder="add comment here"></input>`)
+            $(`#${posts[index].id}`).append(`<button class="comButton">post</button>`)
+            $(`#${posts[index].id}`).append(`<button class="postDel">Delete Post</button>`)
+
         }
     }
     return {
