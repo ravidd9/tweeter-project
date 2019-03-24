b4bc27659a10bcc6852769187fbcb76305e9074f
Ravid Cohen <ravidd9@gmail.com>
2019-03-24 15:18:31 +0200

    24.3 twit

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const tweeter = Tweeter()
 const renderer = Renderer()
-renderer.renderPosts(tweeter.getPosts()) 
+const render = () => renderer.renderPosts(tweeter.getPosts()) 
 
 
-$("#container").on("click", "i", function(){ 
+$("#container").on("click", "i", function(){  //remove comment
     let postID = $(this).closest(".post").attr("id")
     let commentID = $(this).closest(".comment").attr("id")
     tweeter.removeComment(postID,commentID)
-    renderer.renderPosts(tweeter.getPosts())
+    render()
 })
-$("#container").on("click", ".comButton", function(){
+$("#container").on("click", ".comButton", function(){ //add comment
     let text = $(this).closest(".post").find(".comInput").val()
     if (text != ""){
         tweeter.addComment(text, $(this).closest(".post").attr("id"))
-        renderer.renderPosts(tweeter.getPosts())
-        
+        render()
     }
 })
-$("#container").on("click", "#post", function(){
+$("#container").on("click", "#post", function(){ //add post
     let text = $(this).siblings("#input").val()
     console.log("asd")
     if (text != ""){
         tweeter.addPost(text)
-        renderer.renderPosts(tweeter.getPosts())
+        render()
     }
 })
-$("#container").on("click", ".postDel", function(){
+$("#container").on("click", ".postDel", function(){ //remove post
     let postID = $(this).closest(".post").attr("id")
     tweeter.removePost(postID)
-    renderer.renderPosts(tweeter.getPosts())
+    render()
 })
+render()
